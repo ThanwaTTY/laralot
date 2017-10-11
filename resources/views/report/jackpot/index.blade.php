122bd60b947eef2e71667f673a250a145c9a8188
patthanapong <neversay.987@hotmail.com>
2017-10-11 15:37:59 +0700

    รูปแบบ code รายการแพ้/ชนะ สมาชิก/ประเภท

--- resources/views/report/jackpot/index.blade.php
+++ resources/views/report/jackpot/index.blade.php
@@ -60,26 +60,25 @@
     @endif
     
 
-<div class="page-content" id="user-content">
-        <div class="row">
-          <div id="content" class="col-xs-12"><div class="page-header">
-  <h1>
-    รายการที่ถูกรางวัล
-  </h1>
-  <h4 class="sub">
-    <i class="fa fa-angle-right orange"></i>
-    <span class="deep-blue">รัฐบาลไทย</span>, งวดวันที่ 16 ตุลาคม 2560
-    <span class="smaller near-white"><i>(เปลี่ยนได้ที่แถบเมนูด้านบน)</i></span>
-  </h4>
-</div>
-
-
-  <div class="alert alert-info">
-    <h3 class="no-margin">ยังไม่ประกาศผลรางวัล</h3>
-  </div>
-  </div><!-- /.col -->
-        </div><!-- /.row -->
+  <div class="page-content" id="user-content">
+    <div class="row">
+      <div id="content" class="col-xs-12"><div class="page-header">
+        <h1>
+          รายการที่ถูกรางวัล
+        </h1>
+        <h4 class="sub">
+          <i class="fa fa-angle-right orange"></i>
+          <span class="deep-blue">รัฐบาลไทย</span>, งวดวันที่ 16 ตุลาคม 2560
+          <span class="smaller near-white"><i>(เปลี่ยนได้ที่แถบเมนูด้านบน)</i></span>
+        </h4>
       </div>
+
+      <div class="alert alert-info">
+        <h3 class="no-margin">ยังไม่ประกาศผลรางวัล</h3>
+      </div>
+    </div><!-- /.col -->
+  </div><!-- /.row -->
+{{-- </div> --}}
 @endsection
 
  
