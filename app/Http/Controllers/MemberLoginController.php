1acf8545e0d09681140e5edb76c5c61aedc15ea5
ThanwaTTY <to.tanwa@gmail.com>
2017-10-14 17:19:26 +0700

    หน้า login member

--- app/Http/Controllers/MemberLoginController.php
+++ app/Http/Controllers/MemberLoginController.php
@@ -4,16 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AuthController extends Controller
+class MemberLoginController extends Controller
 {
     public function getlogin()
     {
-        return view('login.login');
+        return view('login.memberlogin');
     }
 
     public function postLogin(Request $request)
     {
-        // dd('postlogin');
         $rules = [
             'username' => 'required',
             'password' => 'required'
@@ -22,9 +21,9 @@ class AuthController extends Controller
         $inputs = request()->except(['_token']);
         $this->validate($request, $rules);
         if (auth()->attempt($inputs)) {
-            if (auth()->user()->level != 7) {
+            if (auth()->user()->level == 7) {
                 if (auth()->user()->status == 0) {
-                    return redirect()->intended('/');
+                    return redirect()->intended('/play/bet');
                 } elseif (auth()->user()->status == 1) {
                     session()->flash('massage', 'username ของคุณถูกระงับ');
                     return redirect('/admin/login');
@@ -33,18 +32,12 @@ class AuthController extends Controller
                     return redirect()->intended('/');
                 }
             } else {
-                session()->flash('massage', 'User ไม่มีสิทธิ์เข้าใช้งานหน้านี้');
-                return redirect('/admin/login');
+                session()->flash('massage', 'สำหรับ Member เท่านั้น');
+                return redirect('/login');
             }
         } else {
             session()->flash('massage', 'E-mail or Password not Correct');
-            return redirect('/admin/login');
+            return redirect('/login');
         }
-    }
-
-    public function logout()
-    {
-        auth()->logout();
-        return redirect('/admin/login');
     }
 }
---
+++ resources/views/login/memberlogin.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Login form using HTML5 and CSS3</title>
+  
+  
+  
+      <link rel="stylesheet" href="/../memberlogin/css/style.css">
+<style>
+#content form input[type="reset"] {
+    background: rgb(254,231,154);
+    background: -moz-linear-gradient(top, rgba(254,231,154,1) 0%, rgba(254,193,81,1) 100%);
+    background: -webkit-linear-gradient(top, rgba(254,231,154,1) 0%,rgba(254,193,81,1) 100%);
+    background: -o-linear-gradient(top, rgba(254,231,154,1) 0%,rgba(254,193,81,1) 100%);
+    background: -ms-linear-gradient(top, rgba(254,231,154,1) 0%,rgba(254,193,81,1) 100%);
+    background: linear-gradient(top, rgba(254,231,154,1) 0%,rgba(254,193,81,1) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#fee79a', endColorstr='#fec151',GradientType=0 );
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    -ms-border-radius: 30px;
+    -o-border-radius: 30px;
+    border-radius: 30px;
+    -webkit-box-shadow: 0 1px 0 rgba(255,255,255,0.8) inset;
+    -moz-box-shadow: 0 1px 0 rgba(255,255,255,0.8) inset;
+    -ms-box-shadow: 0 1px 0 rgba(255,255,255,0.8) inset;
+    -o-box-shadow: 0 1px 0 rgba(255,255,255,0.8) inset;
+    box-shadow: 0 1px 0 rgba(255,255,255,0.8) inset;
+    border: 1px solid #D69E31;
+    color: #85592e;
+    cursor: pointer;
+    float: left;
+    font: bold 15px Helvetica, Arial, sans-serif;
+    height: 35px;
+    margin: 20px 0 35px 15px;
+    position: relative;
+    text-shadow: 0 1px 0 rgba(255,255,255,0.5);
+    width: 120px;
+}
+#content form input[type="reset"]:hover {
+    background: rgb(254,193,81);
+    background: -moz-linear-gradient(top, rgba(254,193,81,1) 0%, rgba(254,231,154,1) 100%);
+    background: -webkit-linear-gradient(top, rgba(254,193,81,1) 0%,rgba(254,231,154,1) 100%);
+    background: -o-linear-gradient(top, rgba(254,193,81,1) 0%,rgba(254,231,154,1) 100%);
+    background: -ms-linear-gradient(top, rgba(254,193,81,1) 0%,rgba(254,231,154,1) 100%);
+    background: linear-gradient(top, rgba(254,193,81,1) 0%,rgba(254,231,154,1) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#fec151', endColorstr='#fee79a',GradientType=0 );
+}
+
+</style>
+    <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+    <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
+</head>
+
+<body>
+  <body>
+<div class="container">
+	<section id="content">
+        @if(session()->get('massage'))
+            <div class="box">
+                <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+                    <i class="fa fa-minus"></i></button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+                    <i class="fa fa-times"></i></button>
+                </div>
+                <div class="callout callout-danger">
+                <h4>Fail !</h4>
+
+                <p>{{ session()->get('massage') }}</p>
+            </div>
+            </div>
+        @endif
+		<form action="/login" method="POST">
+        {{ csrf_field() }}
+			<h1>Login Form</h1>
+			<div>
+				<input type="text" placeholder="Username" required="" id="username" name="username" />
+			</div>
+			<div>
+				<input type="password" placeholder="Password" required="" id="password" name="password" />
+			</div>
+			<div>
+				<input type="submit" value="Log in" />
+				<input type="reset" value="Reset" />
+                
+				{{-- <a href="#">Lost your password?</a> --}}
+				{{-- <a href="#">Register</a> --}}
+			</div>
+		</form><!-- form -->
+		<div class="button">
+			<a href="#" width="100px">Download source file</a>
+		</div><!-- button -->
+	</section><!-- content -->
+</div><!-- container -->
+</body>
+  
+    <script  src="/../memberlogin/js/index.js"></script>
+
+</body>
+</html>
