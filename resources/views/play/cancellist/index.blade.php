0288874cb3acf7b022b7e175cdd62acc98a1e7a2
Por <porthachapong@gmail.com>
2017-10-15 12:05:28 +0700

    เเก้ไข cancel tab

--- resources/views/play/cancellist/index.blade.php
+++ resources/views/play/cancellist/index.blade.php
@@ -80,68 +80,14 @@
 @endsection
 @section('content')
 
-<<<<<<< HEAD
-            <div class="alert alert-info invert-select-game-hide mobile-bet-hide hidden" id="news">
-              <button type="button" class="close" data-dismiss="alert">
-                <i class="ace-icon fa fa-times"></i>
-              </button>
-                  <b>ประกาศระบบ:</b> <b>***ประกาศข่าว*** หวยรัฐงวดวันที่ 16 ตุลาคม 2560<br>
-                  บริษัทจะเปิดรับแทงหวยเร็วขึ้นโดยเริ่ม วันที่ 11 ตุลาคม 2560 เวลา 12.00 น. และปิดรับวันที่ 16 ตุลาคม 2560 เวลา 14.45 น. <br>
-                  <br>
-                  และหวยรัฐงวดต่อไปจากนี้จะเปิดรับแทงทันทีหลังผลออก 7 วัน เช่น<br>
-                  งวดวันที่ 1 พ.ย. 60 เริ่มเปิดรับวันที่ 23 ต.ค. 60<br>
-                  งวดวันที่ 16 พ.ย. 60 เริ่มเปิดรับวันที่ 8 พ.ย. 60  <br>
-                  <br>
-                  ระบบลบโพย หลังจากแทงเข้ามาแล้วจะสามารถลบโพยหรือรายการได้ไม่เกินอย่างละ 10 ครั้ง และภายในเวลา 180 นาที นับจากที่แทงเข้ามาแล้ว เกินจากนี้จะไม่สามารถทำการลบได้<br>
-                  <br>
-                  หากมีการปรับเปลี่ยนอะไรเพิ่มเติม ทางบริษัทจะรีบแจ้งข่าวให้ทราบต่อไป ขอบคุณครับ<br>
-                  <br>
-                  </b><br>
-            </div>
-            <div id="content"><h3 class="no-margin-top"><i class="fa fa-angle-right blue"></i> ยกเลิก โพย/รายการแทง</h3>
-          <ul class="nav nav-tabs padding-12 tab-color-blue background-blue">
-            <li class="active">
-              <a href="/play/cancellist" class="ajax">ยกเลิก โพย/รายการแทง</a>
-            </li>
-
-            <li>
-              <a href="/play/cancellist/cancellist" class="ajax">รายการที่ถูกยกเลิก</a>
-            </li>
-          </ul>
-
-<style>
-  #search-table td {
-    padding: 0 10px 4px 0;
-  }
-</style>
-
-<div class="space-4"></div>
-  <form method="GET" action="" accept-charset="UTF-8" class="form-inline form-cancel-search">
-  <div class="widget-box widget-color-blue2 width-800 max-width-800">
-    <div class="widget-header">
-      <h5 class="widget-title bigger">
-        ค้นหา โพย/รายการแทง
-      </h5>
-    </div>
-    <div class="widget-body">
-      <div class="widget-main">
-        <div class="row clearfix">
-          <div class="col-xs-6 pull-left">
-            <span class="bigger-110">ยกเลิกโพยได้: <span class="deep-blue">10</span> ครั้ง/งวด</span><br>
-            <span class="bigger-110">ยกเลิกรายการแทงได้: <span class="deep-blue">10</span> ครั้ง/งวด</span><br>
-          </div>
-          <div class="col-xs-6 pull-left">
-            <span class="bigger-110">ต้องยกเลิกภายใน <span class="deep-blue">180</span> นาที หลังแทงหวย</span><br>
-            <span class="bigger-110">ต้องยกเลิกก่อนหวยปิดงวด <span class="deep-blue">15</span> นาที</span><br>
-=======
 <div id="content"><h3 class="no-margin-top"><i class="fa fa-angle-right blue"></i> ยกเลิก โพย/รายการแทง</h3>
       <ul class="nav nav-tabs padding-12 tab-color-blue background-blue">
         <li class="active">
-          <a href="#" class="ajax">ยกเลิก โพย/รายการแทง</a>
+          <a href="/play/cancellist" class="ajax">ยกเลิก โพย/รายการแทง</a>
         </li>
 
         <li>
-          <a href="#" class="ajax">รายการที่ถูกยกเลิก</a>
+          <a href="/play/cancellist/cancellist" class="ajax">รายการที่ถูกยกเลิก</a>
         </li>
       </ul>
 
@@ -152,7 +98,7 @@
   </style>
 
 <div class="space-4"></div>
-  <form method="GET" action="https://www.superlot999.com/cancel/search" accept-charset="UTF-8" class="form-inline form-cancel-search">
+  <form method="GET" action="#" accept-charset="UTF-8" class="form-inline form-cancel-search">
     <div class="widget-box widget-color-blue2 width-800 max-width-800">
       <div class="widget-header">
         <h5 class="widget-title bigger">
@@ -170,7 +116,6 @@
               <span class="bigger-110">ต้องยกเลิกภายใน <span class="deep-blue">180</span> นาที หลังแทงหวย</span><br>
               <span class="bigger-110">ต้องยกเลิกก่อนหวยปิดงวด <span class="deep-blue">15</span> นาที</span><br>
             </div>
->>>>>>> fe585f8a5959c4c9265e1658f37e98dd7f4b7d5d
           </div>
           <div class="space-4"></div>
           <hr class="hr-4">
@@ -203,42 +148,9 @@
             </tbody>
           </table>
         </div>
-<<<<<<< HEAD
-        <div class="space-4"></div>
-        <hr class="hr-4">
-        <span class="grey"><b>ลำดับโพย และ หมายเลข</b> สามารถกรอกหลายตัว คั่นด้วยคอมม่า เช่น 13,4,555</span>
-        <div class="space-4"></div>
-        <table id="search-table">
-          <tbody>
-            <tr>
-              <td><label for="daterange" class="deep-blue bolder">วันที่แทง :</label></td>
-              <td><input class="form-control width-100 bigger-110 date-range-picker" name="daterange" type="text" value="12/10/2017 - 12/10/2017" id="daterange"></td>
-            </tr>
-            <tr>
-              <td><label for="search_type" class="deep-blue bolder">ค้นหา :</label></td>
-              <td><select class="form-control width-100 bigger-110" id="search_type" name="search_type"><option value="gb" selected="selected">โพย</option><option value="gbbl">รายการแทง</option></select></td>
-              <td><label for="username" class="deep-blue bolder">ชื่อผู้ใช้ :</label></td>
-              <td><input class="form-control bigger-110" id="username" readonly="readonly" name="username" type="text" value="{{auth()->user()->username}}"></td>
-            </tr>
-            <tr>
-              <td><label for="game_bill_numbers" class="bolder">ลำดับโพย :</label></td>
-              <td><input class="form-control bigger-110" id="game_bill_numbers" name="game_bill_numbers" type="text" value=""></td>
-              <td><label for="numbers" class="bolder">หมายเลขที่แทง :</label></td>
-              <td><input class="form-control bigger-110" id="number" name="numbers" type="text" value=""></td>
-            </tr>
-            <tr>
-              <td><label for="remark" class="bolder">หมายเหตุ :</label></td>
-              <td><input class="form-control bigger-110" name="remark" type="text" value="" id="remark"></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-=======
       </div>
       <div class="widget-toolbox padding-8 clearfix">
         <button type="submit" class="btn btn-primary btn-sm" id="btn-search-cancel">ค้นหา</button>
->>>>>>> fe585f8a5959c4c9265e1658f37e98dd7f4b7d5d
       </div>
     </div>
   </form>
